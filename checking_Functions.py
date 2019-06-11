de0fcece5e73a4b84a01c08c1ac4b722160e8a60
cacsphysics <ccartagena@brynmawr.edu>
2019-06-11 18:50:55 -0400

    Update checking_Functions.py

--- checking_Functions.py
+++ checking_Functions.py
@@ -3,7 +3,7 @@ import BMX
 import matplotlib.pylab as plt
 
 filename = '20190423-0001 (17).txt'
-data1, data2, time, data3, data4 = BMX.BMX_Magnetic_BDOT(filename, data_Structure = 'ztzt')
+data1, data2, time, data3, data4 = BMX.BMX_Magnetic_BDOT(filename, data_Structure = 'tztz')
 #name = 'ztzt'
 #data = BMX.BMX_Pico_Read(filename)
 #my_Dict = {'%s'%filename: filename}
